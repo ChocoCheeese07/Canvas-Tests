768f96e183d28870cb0120a1517a35fdf0b31872
ChocolateCheese9 <crazycrab119@gmail.com>
2019-09-12 16:51:10 -0700

    cfh

--- scripts/index.js
+++ scripts/index.js
@@ -1,4 +1,15 @@
 var canvas = document.getElementById("view");
-canvas.width = innerWidth;
-canvas.height = innerHeight;
+canvas.width = 700;
+canvas.height = 400;
 var ctx = canvas.getContext("2d");
+
+function update() {
+ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+var posX = 5;
+ctx.fillRect(posX, 20, 50, 50);
+posX += 6;
+
+requestAnimationFrame(update)
+}
+requestAnimationFrame(update);
