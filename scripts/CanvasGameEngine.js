1c55ff7c244bc6720a8944e66b8d218707df7e8a
ChocolateCheese9 <crazycrab119@gmail.com>
2019-09-16 17:38:43 -0700

    ggghufv

---
+++ scripts/CanvasGameEngine.js
@@ -0,0 +1,82 @@
+var CanvasGameEngine = {
+Rect : class Rect {
+constructor(posX, posY, velX, velY, w, h, color) {
+this.canvas = document.getElementById("view");
+this.ctx = this.canvas.getContext("2d");
+this.pos = {x: posX, y: posY};
+this.vel = {x: velX, y: velY};
+this.color = color;
+this.width = w;
+this.height = h;
+}
+Draw() {
+this.ctx.fillStyle = this.color;
+this.ctx.fillRect(this.pos.x, this.pos.y, this.width, this.height);
+}
+CheckEdges() {
+if(this.pos.x + this.width > this.canvas.width) {
+this.pos.x = this.canvas.width - this.width;
+this.vel.x = -this.vel.x;
+};
+if(this.pos.x < 0) {
+this.pos.x = 0;
+this.vel.x = -this.vel.x;
+};
+if(this.pos.y + this.height > this.canvas.height) {
+this.pos.y = this.canvas.height - this.height;
+this.vel.y = -this.vel.y;
+};
+if(this.pos.y < 0) {
+this.pos.y = 0;
+this.vel.y = -this.vel.y;
+};
+}
+Update() {
+this.pos.x += this.vel.x;
+this.pos.y += this.vel.y;
+this.CheckEdges();
+this.Draw();
+}
+},
+Circle : class Circle {
+constructor(posX, posY, velX, velY, radius, color) {
+this.canvas = document.getElementById("view");
+this.ctx = this.canvas.getContext("2d");
+this.pos = {x: posX, y: posY};
+this.vel = {x: velX, y: velY};
+this.color = color;
+this.radius = radius;
+}
+Draw() {
+this.ctx.fillStyle = this.color;
+this.ctx.beginPath();
+this.ctx.arc(this.pos.x, this.pos.y, this.radius, 0, Math.PI * 2);
+this.ctx.fill();
+this.ctx.closePath();
+}
+CheckEdges() {
+if(this.pos.x + this.radius > this.canvas.width) {
+this.pos.x = this.canvas.width - this.radius;
+this.vel.x = -this.vel.x;
+}
+if(this.pos.x - this.radius < 0) {
+this.pos.x = this.radius;
+this.vel.x = -this.vel.x;
+}
+if(this.pos.y + this.radius > this.canvas.height) {
+this.pos.y = this.canvas.height - this.radius;
+this.vel.y = -this.vel.y;
+}
+if(this.pos.y - this.radius < 0) {
+this.pos.y = this.radius;
+this.vel.y = -this.vel.y;
+}
+}
+Update() {
+this.pos.x += this.vel.x;
+this.pos.y += this.vel.y;
+this.CheckEdges();
+this.Draw();
+}
+}
+}
--- scripts/index.js
+++ scripts/index.js
@@ -2,13 +2,21 @@ var canvas = document.getElementById("view");
 canvas.width = 700;
 canvas.height = 400;
 var ctx = canvas.getContext("2d");
+var circles = [];
+function randomInt(max) {
+return Math.floor(Math.random() * max);
+}
 
-var rect = new Rect(10, 20, 3, 2, 50, 50, "#d1e617");
+for(let i = 0; i < 500; i++) {
+circles.push(new CanvasGameEngine.Circle(
+randomInt(canvas.width),
+randomInt(canvas.height),
+
+));
+}
 
 function update() {
 ctx.clearRect(0, 0, canvas.width, canvas.height);
-
-rect.Update();
 
 requestAnimationFrame(update)
 }
