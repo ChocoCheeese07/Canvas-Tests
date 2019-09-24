ca3c78b9bb0d87d4355a3298f310b1425fbf57c8
ChocolateCheese9 <31376905+ChocolateCheese9@users.noreply.github.com>
2019-09-24 14:45:50 -0700

    q

--- Tests/GravityFriction/scripts/index.js
+++ Tests/GravityFriction/scripts/index.js
@@ -7,7 +7,7 @@ var ctx = canvas.getContext("2d");
 function randomInt(max) {
 return Math.floor(Math.random() * max);
 }
-
+var test = new CanvasGameEngine.Rect(0, 100, 3, -6, 50, 50, "#3e8db8");
 function update() {
 ctx.clearRect(0, 0, canvas.width, canvas.height);
 
