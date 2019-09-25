8c4a8d348cc9971dfe60ade8cb88e8fc0e37b5f9
ChocolateCheese9 <31376905+ChocolateCheese9@users.noreply.github.com>
2019-09-25 09:17:21 -0700

    Q

--- Tests/BouncingEpilepsy/scripts/index.js
+++ Tests/BouncingEpilepsy/scripts/index.js
@@ -5,12 +5,9 @@ canvas.id = "view";
 document.body.appendChild(canvas);
 var ctx = canvas.getContext("2d");
 var circles = [];
-function randomInt(max) {
-return Math.floor(Math.random() * max);
-}
 
 for(let i = 0; i < 1000; i++) {
-circles.push(new CanvasGameEngine.Circle(randomInt(canvas.width), randomInt(canvas.height), randomInt(4) + 2, randomInt(4) + 2, randomInt(30) + 20, `hsl(${randomInt(360)}, ${randomInt(100)}%, ${randomInt(100)}%)`));
+circles.push(new CanvasGameEngine.Circle(CanvasGameEngine.randomInt(0, canvas.width), CanvasGameEngine.randomInt(0, canvas.height), CanvasGameEngine.randomInt(-4, 4), CanvasGameEngine.randomInt(-4, 4), CanvasGameEngine.randomInt(15, 40), `hsl(${CanvasGameEngine.randomInt(0, 360)}, ${CanvasGameEngine.randomInt(0, 100)}%, ${CanvasGameEngine.randomInt(0, 100)}%)`));
 circles[i].frictionEnabled = false;
 }
 
@@ -20,7 +17,7 @@ ctx.clearRect(0, 0, canvas.width, canvas.height);
 // ctx.fillRect(0, 0, canvas.width, canvas.height);
 
 for(i in circles) {
-    circles[i].color = `hsl(${randomInt(360)}, ${randomInt(100)}%, ${randomInt(100)}%)`;
+    circles[i].color = `hsl(${CanvasGameEngine.randomInt(0, 360)}, ${CanvasGameEngine.randomInt(0, 100)}%, ${CanvasGameEngine.randomInt(0, 100)}%)`;
     circles[i].Update();
 }
 
