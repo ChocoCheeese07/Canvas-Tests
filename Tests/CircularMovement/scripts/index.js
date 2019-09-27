a3fce96692d143f5c00c1ef02a44184d4e0b0341
ChocolateCheese9 <crazycrab119@gmail.com>
2019-09-27 16:44:42 -0700

    q

--- Tests/CircularMovement/scripts/index.js
+++ Tests/CircularMovement/scripts/index.js
@@ -10,7 +10,9 @@ var r = 125;
 var rect1 = new CanvasGameEngine.Rect(canvas.width / 2 + Math.cos(angle) * r, canvas.height / 2 + Math.sin(angle) * r, 0, 0, 100, 100, "red");
 
 function update() {
-ctx.clearRect(0, 0, canvas.width, canvas.height);
+// ctx.clearRect(0, 0, canvas.width, canvas.height);
+ctx.fillStyle = "rgba(173, 216, 230, .1)";
+ctx.fillRect(0, 0, canvas.width, canvas.height);
 
 angle += .1;
 rect1.pos.x = canvas.width / 2 + Math.cos(angle) * r;
