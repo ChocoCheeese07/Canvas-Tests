c2fe849b766c67ab1b15101eb5f8358dd02a26b2
ChocolateCheese9 <crazycrab119@gmail.com>
2019-09-27 12:20:56 -0700

    Quick Commit

--- Tests/CircularMovement/scripts/index.js
+++ Tests/CircularMovement/scripts/index.js
@@ -1,1 +1,17 @@
+var canvas = document.createElement("canvas");
+canvas.width = innerWidth;
+canvas.height = innerHeight;
+canvas.id = "view";
+document.body.appendChild(canvas);
+var ctx = canvas.getContext("2d");
 
+var rect1 = new CanvasGameEngine.Rect(canvas.width / 2, canvas.height / 2, 0, 0, 100, 100, "red");
+
+function update() {
+ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+rect1.Update();
+
+requestAnimationFrame(update)
+}
+requestAnimationFrame(update)
