24d2809495c355dc931dc3dae2f7e7d4e1cc378c
ChocolateCheese9 <crazycrab119@gmail.com>
2019-09-27 16:29:37 -0700

    q

--- Tests/CircularMovement/scripts/index.js
+++ Tests/CircularMovement/scripts/index.js
@@ -5,11 +5,16 @@ canvas.id = "view";
 document.body.appendChild(canvas);
 var ctx = canvas.getContext("2d");
 
-var rect1 = new CanvasGameEngine.Rect(canvas.width / 2, canvas.height / 2, 0, 0, 100, 100, "red");
+var angle = 0;
+var r = 50;
+var rect1 = new CanvasGameEngine.Rect(canvas.width / 2 + Math.cos(angle) * r, canvas.height / 2 + Math.sin(angle) * r, 0, 0, 100, 100, "red");
 
 function update() {
 ctx.clearRect(0, 0, canvas.width, canvas.height);
 
+angle += .1;
+rect1.pos.x = canvas.width / 2 + Math.cos(angle) * r;
+rect1.pos.y = canvas.height / 2 + Math.sin(angle) * r;
 rect1.Update();
 
 requestAnimationFrame(update)
