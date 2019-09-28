6104014e930ce6759d55ce563737af1d0f6de5de
ChocolateCheese9 <crazycrab119@gmail.com>
2019-09-28 11:13:46 -0700

    changed speeds

--- Tests/CircularMovement/scripts/index.js
+++ Tests/CircularMovement/scripts/index.js
@@ -17,30 +17,34 @@ rect3.pathRadius = 170;
 var rect4 = new CanvasGameEngine.Rect(canvas.width / 2, canvas.height / 2, 0, 0, 50, 50, "red");
 rect4.angle = 0;
 rect4.pathRadius = 230;
-
-
-
+var rect5 = new CanvasGameEngine.Rect(canvas.width / 2, canvas.height / 2, 0, 0, 50, 50, "blue");
+rect5.angle = Math.PI;
+rect5.pathRadius = 290;
 
 function update() {
     // ctx.clearRect(0, 0, canvas.width, canvas.height);
     ctx.fillStyle = "rgba(173, 216, 230, .1)";
     ctx.fillRect(0, 0, canvas.width, canvas.height);
 
-    rect1.angle += -.1;
+    rect1.angle += -.06;
     rect1.pos.x = canvas.width / 2 + Math.cos(rect1.angle) * rect1.pathRadius;
     rect1.pos.y = canvas.height / 2 + Math.sin(rect1.angle) * rect1.pathRadius;
 
-    rect2.angle += .1;
+    rect2.angle += .06;
     rect2.pos.x = canvas.width / 2 + Math.cos(rect2.angle) * rect2.pathRadius;
     rect2.pos.y = canvas.height / 2 + Math.sin(rect2.angle) * rect2.pathRadius;
 
-    rect3.angle += -.1;
+    rect3.angle += -.06;
     rect3.pos.x = canvas.width / 2 + Math.cos(rect3.angle) * rect3.pathRadius;
     rect3.pos.y = canvas.height / 2 + Math.sin(rect3.angle) * rect3.pathRadius;
 
-    rect4.angle += .1;
+    rect4.angle += .06;
     rect4.pos.x = canvas.width / 2 + Math.cos(rect4.angle) * rect4.pathRadius;
     rect4.pos.y = canvas.height / 2 + Math.sin(rect4.angle) * rect4.pathRadius;
+    
+    rect5.angle += -.06;
+    rect5.pos.x = canvas.width / 2 + Math.cos(rect3.angle) * rect3.pathRadius;
+    rect3.pos.y = canvas.height / 2 + Math.sin(rect3.angle) * rect3.pathRadius;
 
     rect1.Update();
     rect2.Update();
