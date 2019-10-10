ac4dae2476e08706626dd756c528cbc4ffaa755e
ChocolateCheese9 <crazycrab119@gmail.com>
2019-10-10 07:46:52 -0700

    ss

--- Tests/CollisionTests/scripts/index.js
+++ Tests/CollisionTests/scripts/index.js
@@ -11,21 +11,21 @@ return Math.floor(Math.random() * max);
 var center = new CGE.Rect(canvas.width / 2, canvas.height / 2, 0,  0, 50, 50, "red");
 var left = new CGE.Rect(0, canvas.height / 2, 2,  0, 50, 50, "blue");
 var right = new CGE.Rect(canvas.width - 25, canvas.height / 2, -2,  0, 50, 50, "blue");
-var top = new CGE.Rect(canvas.width / 2, 0, 0,  2, 50, 50, "blue");
+var above = new CGE.Rect(canvas.width / 2, 0, 0, 2, 50, 50, "blue");
 var bottom = new CGE.Rect(canvas.width / 2, canvas.height, 0,  -2, 50, 50, "blue");
 
 function update() {
 ctx.clearRect(0, 0, canvas.width, canvas.height);
 
-CGE.RectRectCollision(center, left);
-CGE.RectRectCollision(center, right);
-CGE.RectRectCollision(center, top);
-CGE.RectRectCollision(center, bottom);
+CGE.RectRectCollision(left, center);
+CGE.RectRectCollision(right, center);
+CGE.RectRectCollision(above, center);
+CGE.RectRectCollision(bottom, center);
 
 center.Update();
 left.Update();
 right.Update();
-top.Update();
+above.Update();
 bottom.Update();
   
 requestAnimationFrame(update)
