3c46f61e470e3ee3600b78c404d05868932e1d7e
ChocolateCheese9 <31376905+ChocolateCheese9@users.noreply.github.com>
2019-10-11 14:52:31 -0700

    Update index.js

--- Tests/CollisionTests/scripts/index.js
+++ Tests/CollisionTests/scripts/index.js
@@ -13,12 +13,9 @@ var rects = [
   new CGE.Rect(canvas.width, canvas.height, 2, 2, 50, 50, "blue"),
   new CGE.Rect(0, canvas.height, 2, 2, 50, 50, "blue")
 ]
-//var left = new CGE.Rect(0, 0, 2,  2, 50, 50, "blue");
-//var right = new CGE.Rect(canvas.width, 0, 2, 2, 50, 50, "blue");
-//var above = new CGE.Rect(canvas.width, canvas.height, 2, 2, 50, 50, "blue");
-//var bottom = new CGE.Rect(0, canvas.height, 2, 2, 50, 50, "blue");
-var circle1 = new CGE.Circle(0, 0, 2, 2, 50, "red");
-var circle2 = new CGE.Circle(canvas.width, 0, 2, 2, 50, "red");
+
+var circle1 = new CGE.Circle(canvas.width / 4, 0, 2, 2, 50, "red");
+var circle2 = new CGE.Circle(canvas.width - canvas.width / 4, 0, 2, 2, 50, "red");
 function checkAllRectCollisions(array = []) {
   for(let i in array) {
     for(let j in array) {
@@ -34,13 +31,6 @@ CGE.CircleCircleCollision(circle1, circle2);
 circle1.Update();
 circle2.Update();
 
-//CGE.RectRectCollision(left, right);
-//CGE.RectRectCollision(left, above);
-//CGE.RectRectCollision(left, bottom);
-//CGE.RectRectCollision(left, right);
-//CGE.RectRectCollision(right, above);
-//CGE.RectRectCollision(left, bottom);
-//CGE.RectRectCollision(above, bottom);
 checkAllRectCollisions(rects);
 for(let i in rects) {
   rects[i].Update();
