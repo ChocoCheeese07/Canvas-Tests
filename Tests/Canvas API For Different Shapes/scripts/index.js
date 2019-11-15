0f81df049a911b23f48f0f21dc4cd2718abadbfa
ChocolateCheese9 <31376905+ChocolateCheese9@users.noreply.github.com>
2019-11-15 11:35:28 -0800

    Update index.js

--- Tests/Canvas API For Different Shapes/scripts/index.js
+++ Tests/Canvas API For Different Shapes/scripts/index.js
@@ -1,1 +1,7 @@
+var canvas = document.createElement("canvas");
+var ctx = canvas.getContext("2d");
+canvas.width = innerWidth;
+canvas.height = innerHeight;
+document.body.appendChild(canvas);
 
+ctx.fillRect(5, 5, 40, 40);
